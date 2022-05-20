f33c8e9a60f62900011f983eee9aec4de41e2be1
Cameron <c-em@outlook.com>
2022-05-20 12:40:48 -0700

    Refined guess attempt countdown to only decrease with incorrect guesses

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -7,20 +7,25 @@ guesses = set()
 attempts = 8
 game_over = False
 
-print(f"H A N G M A N # {attempts} attempts\n")
+print(f"H A N G M A N # {attempts} attempts")
 
 while attempts > 0 and not game_over:
-    print(hangman_functions.show_hidden_word(correct_word, guesses))
+    print("\n" + hangman_functions.show_hidden_word(correct_word, guesses))
     guess = input("Input a letter: ").lower()
-    guesses.add(guess)
-    attempts -= 1
     if len(guess) != 1 or guess not in correct_word:
-        print(f"\nThat letter doesn't appear in the word.  # {attempts} attempts\n")
-    elif guess in correct_word:
-        print(f" # {attempts} attempts\n")
-        # if set(guesses).issuperset(correct_word):
-        #     game_over = True
+        guesses.add(guess)
+        attempts -= 1
+        print(f"That letter doesn't appear in the word.  # {attempts} attempts")
+    elif guess in correct_word and guess not in guesses:
+        guesses.add(guess)
+        if set(guesses).issuperset(correct_word):
+            game_over = True
+            print("\n" + correct_word)
+            print("You guessed the word!")
+    elif guess in correct_word and guess in guesses:
+        attempts -= 1
+        print(f"No improvements.  # {attempts} attempts")
 
-print("Thanks for playing!")
+print("You survived!" if attempts > 0 else "\nYou lost!")
 
 
