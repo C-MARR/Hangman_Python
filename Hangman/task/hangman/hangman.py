b29921307dee3655db1b2003699ecdba2ce130d5
Cameron <c-em@outlook.com>
2022-05-20 10:59:54 -0700

    Added word clue

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -3,5 +3,11 @@ import random
 print("H A N G M A N")
 words = ("python", "java", "swift", "javascript")
 correct_word = random.choice(words)
-guess = input("Guess the word: ")
+hidden_word = []
+for char in range(len(correct_word)):
+    if char > 2:
+        hidden_word.append("-")
+    else:
+        hidden_word.append(correct_word[char])
+guess = input(f'Guess the word: {"".join(hidden_word)}')
 print("You survived!" if guess == correct_word else "You lost!")
