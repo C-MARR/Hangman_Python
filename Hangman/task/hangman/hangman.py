1f1164001b8385c4a6340319f4d25fd5fb35ac9d
Cameron <c-em@outlook.com>
2022-05-20 12:56:15 -0700

    Added invalid entry error prompts

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -11,20 +11,23 @@ print(f"H A N G M A N # {attempts} attempts")
 
 while attempts > 0 and not game_over:
     print("\n" + hangman_functions.show_hidden_word(correct_word, guesses))
-    guess = input("Input a letter: ").lower()
-    if len(guess) != 1 or guess not in correct_word:
-        guesses.add(guess)
-        attempts -= 1
-        print(f"That letter doesn't appear in the word.  # {attempts} attempts")
-    elif guess in correct_word and guess not in guesses:
-        guesses.add(guess)
-        if set(guesses).issuperset(correct_word):
-            game_over = True
-            print("\n" + correct_word)
-            print("You guessed the word!")
-    elif guess in correct_word and guess in guesses:
-        attempts -= 1
-        print(f"No improvements.  # {attempts} attempts")
+    guess = input("Input a letter: ")
+    if not guess.islower() and len(guess) == 1:
+        print("Please, enter a lowercase letter from the English alphabet.")
+    elif len(guess) != 1:
+        print("Please, input a single letter")
+    else:
+        if guess in correct_word and guess in guesses:
+            print("You've already guessed this letter.")
+        elif guess not in correct_word:
+            guesses.add(guess)
+            attempts -= 1
+            print(f"That letter doesn't appear in the word.  # {attempts} attempts")
+        elif guess in correct_word and guess not in guesses:
+            guesses.add(guess)
+            if set(guesses).issuperset(correct_word):
+                game_over = True
+                print(f"You guessed the word {correct_word}!")
 
 print("You survived!" if attempts > 0 else "\nYou lost!")
 
