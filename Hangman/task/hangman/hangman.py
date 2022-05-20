a5a17a05713d772db4e31f1a299e424e71b7a100
Cameron <c-em@outlook.com>
2022-05-20 08:12:17 -0700

    Added multiple words to randomly guess

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,3 +1,7 @@
+import random
+
 print("H A N G M A N")
+words = ("python", "java", "swift", "javascript")
+correct_word = random.choice(words)
 guess = input("Guess the word: ")
-print("You survived!" if guess == "python" else "You lost!")
+print("You survived!" if guess == correct_word else "You lost!")
